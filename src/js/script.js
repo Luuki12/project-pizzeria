d191481b44b29a6eb4db164a29daf52d943ec145
Łukasz Kidoń <lkidon4@gmail.com>
2021-03-02 16:22:35 +0100

    initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -62,6 +62,7 @@ const { utils } = require("stylelint");
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
 
       console.log('new Product', thisProduct);
     }
@@ -79,6 +80,26 @@ const { utils } = require("stylelint");
 
       //add element to menu
       menuContainer = appendChild(thisProduct.element);
+    }
+
+    initAccordion(){
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = select.menuProduct.clickable;
+
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+        /* prevent default action for event */
+        event.preventDefault();
+
+        /* find active product (product that has active class) */
+
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+
+        /* toggle active class on thisProduct.element */
+      });
+
     }
   }
 
