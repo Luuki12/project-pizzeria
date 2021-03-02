f7fccddd1f68e898f8bc6bb57417f626efe96daf
Łukasz Kidoń <lkidon4@gmail.com>
2021-03-02 21:01:39 +0100

    initOrderForm to fix

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,4 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
-
-const { utils } = require("stylelint");
-
 {
   'use strict';
 
@@ -63,30 +60,37 @@ const { utils } = require("stylelint");
 
       thisProduct.renderInMenu();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
+      thisProduct.getElements();
 
       console.log('new Product', thisProduct);
     }
     renderInMenu(){
       const thisProduct = this;
 
-      //generate HTML based on template
       const generatedHTML = templates.menuProduct(thisProduct.data);
 
-      //create element using utils.createElementFromHTML
       thisProduct.element = utils.createDOMFromHTML(generatedHTML);
 
-      //find menu container
       const menuContainer = document.querySelector(select.containerOf.menu);
 
-      //add element to menu
-      menuContainer = appendChild(thisProduct.element);
+      menuContainer.appendChild(thisProduct.element);
+    }
+    getElements(){
+      const thisProduct = this;
+
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
     }
 
     initAccordion(){
       const thisProduct = this;
-
       /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = select.menuProduct.clickable;
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
 
       /* START: add event listener to clickable trigger on event click */
       clickableTrigger.addEventListener('click', function(event) {
@@ -94,12 +98,51 @@ const { utils } = require("stylelint");
         event.preventDefault();
 
         /* find active product (product that has active class) */
+        const activeProducts = document.querySelectorAll(select.all.menuProductsActive);
 
         /* if there is active product and it's not thisProduct.element, remove class active from it */
 
+        for (const activeProduct of activeProducts) {
+          if (activeProduct !== thisProduct.element) {
+            activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+          }
+        }
+
         /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
 
+    }
+
+    initOrderForm(){
+      const thisProduct = this;
+
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      console.log('orderForm', thisProduct);
+    }
+
+    processOrder(){
+      const thisProduct = this;
+
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
+
+      console.log('processOrder', thisProduct);
     }
   }
 
