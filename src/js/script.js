68eb87f8fd77661f140cc21a2ea37eb76fbbe719
Łukasz Kidoń <lkidon4@gmail.com>
2021-03-04 13:53:08 +0100

    add image to func. processOrder

--- src/js/script.js
+++ src/js/script.js
@@ -68,12 +68,10 @@
     }
     renderInMenu(){
       const thisProduct = this;
-
       const generatedHTML = templates.menuProduct(thisProduct.data);
+      const menuContainer = document.querySelector(select.containerOf.menu);
 
       thisProduct.element = utils.createDOMFromHTML(generatedHTML);
-
-      const menuContainer = document.querySelector(select.containerOf.menu);
 
       menuContainer.appendChild(thisProduct.element);
     }
@@ -85,22 +83,17 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion(){
       const thisProduct = this;
-      /* find the clickable trigger (the element that should react to clicking) */
       const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
 
-      /* START: add event listener to clickable trigger on event click */
       clickableTrigger.addEventListener('click', function(event) {
-        /* prevent default action for event */
         event.preventDefault();
 
-        /* find active product (product that has active class) */
         const activeProducts = document.querySelectorAll(select.all.menuProductsActive);
-
-        /* if there is active product and it's not thisProduct.element, remove class active from it */
 
         for (const activeProduct of activeProducts) {
           if (activeProduct !== thisProduct.element) {
@@ -108,7 +101,6 @@
           }
         }
 
-        /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
 
@@ -138,45 +130,37 @@
 
     processOrder() {
       const thisProduct = this;
-
-      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log('formData', formData);
 
-      // set price to default price
       let price = thisProduct.data.price;
 
-      // for every category (param)...
       for(let paramId in thisProduct.data.params) {
-        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
         console.log(paramId, param);
 
-        // for every option in this category
         for(let optionId in param.options) {
-          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          console.log(optionId, option);
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
 
-          // check if there is param with a name of paramId in formData and if it includes optionId
-          if(formData[paramId] && formData[paramId].includes(optionId)) {
-            // check if the option is not default
+          if(optionSelected){
             if(!option.default == true) {
-              // add option price to price variable
               price += option.price;
             }
-          } else {
-            // check if the option is default
-            if(option.default == true) {
-              // reduce price variable
-              price -= option.price;
+          } else if(option.default == true) {
+            price -= option.price;
+          }
+
+          if(optionImage){
+            if(optionSelected){
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            } else if(!optionSelected) {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
             }
           }
 
         }
       }
-
-      // update calculated price in the HTMLm
       thisProduct.priceElem.innerHTML = price;
     }
   }
